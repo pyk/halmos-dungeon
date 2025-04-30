c5839420109b299cc6ee256cf5b603c537a42bf1
pyk <2213646+pyk@users.noreply.github.com>
2025-04-30 16:51:01 +0700

    chore: use explicit last path

--- test/L4_PiggyBankBuggy.t.sol
+++ test/L4_PiggyBankBuggy.t.sol
@@ -27,6 +27,7 @@ contract PiggyBankBuggyTest is SymTest, Test {
 
         uint256 userDepositAmount = svm.createUint256("user_deposit_amount");
         uint256 attackerDepositAmount = svm.createUint256("attacker_deposit_amount");
+        uint256 attackerWithdrawAmount = svm.createUint256("attacker_withdraw_amount");
 
         // Assume user & attacker have deposits
         vm.assume(token.balanceOf(user) == userDepositAmount);
@@ -34,39 +35,22 @@ contract PiggyBankBuggyTest is SymTest, Test {
 
         vm.assume(token.balanceOf(attacker) == attackerDepositAmount);
         vm.assume(target.getDeposit(attacker) == attackerDepositAmount);
+        vm.assume(attackerWithdrawAmount <= userDepositAmount + attackerDepositAmount);
 
         vm.assume(token.balanceOf(address(target)) == userDepositAmount + attackerDepositAmount);
 
         // Lets attacker explore all possible paths to gain more token
         uint256 maxCalls = 10;
-        bytes memory lastCallData;
         for (uint256 i = 0; i < maxCalls; i++) {
             vm.startPrank(attacker);
-            lastCallData = svm.createCalldata("PiggyBankBuggy");
-            (bool success,) = address(target).call(lastCallData);
+            (bool success,) = address(target).call(svm.createCalldata("PiggyBankBuggy"));
             vm.stopPrank();
             vm.assume(success);
         }
 
-        // TODO: I want to validate whether last call is PiggyBankBuggy.withdraw
-        bytes4 selector;
-        if (lastCallData.length >= 4) {
-            assembly {
-                // lastCallData holds the memory address (pointer) of the array.
-                // The actual data starts 32 bytes *after* this pointer (skip the length field).
-                // Load the first 32 bytes of the data.
-                let word := mload(add(lastCallData, 0x20))
-                // Assign the first 4 bytes (most significant) to the bytes4 variable.
-                // Casting from bytes32 to bytes4 takes the high-order bytes.
-                selector := word
-            }
-            // 'selector' now holds the first 4 bytes.
-        } else {
-            // Handle the case where the data is too short (e.g., revert or set default)
-            revert("Data too short to extract selector");
-        }
-
-        vm.assume(selector == PiggyBankBuggy.withdraw.selector);
+        vm.startPrank(attacker);
+        target.withdraw(attackerWithdrawAmount);
+        vm.stopPrank();
 
         assertEq(token.balanceOf(attacker), attackerDepositAmount);
     }
